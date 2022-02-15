52580bf1d1c8682650bb545099c42a6617b55e48
RocioRM <mayenrocio040@gmail.com>
2022-02-15 17:58:01 -0600

    to do list

---
+++ toDo.js
@@ -0,0 +1,33 @@
+const txbInputTask = document.getElementById('txb-input-task')
+const btnAddTask = document.getElementById('btn-add-task')
+const toDoContainer = document.getElementById('to-do-container')
+
+btnAddTask.addEvenListener('click',addTask)
+
+function test(){
+    alert('nice')
+}
+
+function addTask(){
+    let toDoItem = document.createElement('p')
+    toDoItem.innerHTML = txbInputTask.value;
+    toDoContainer.appendChild(toDoItem)
+    toDoItem.addEventListener('click',markTask)
+    toDoItem.addEventListener('dblclick',deleteTask)
+    txbInputTask.value = ''
+}
+
+function markTask(){
+    if(this.style.textDecoration == "line-through"){
+       this.style.textDecoration="none"
+    } else {
+        this.style.textDecoration="line-through"
+    }
+
+}
+
+function deleteTask(){
+    toDoContainer.removeChild(this)
+}
+
+
